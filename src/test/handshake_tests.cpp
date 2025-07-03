7cc4747c9177537354fb285564932761d81546bf
Mr-Miner <127097345+MrMiner-org@users.noreply.github.com>
2025-07-03 11:33:09 +0200

    Implement BIP324 handshake with encryption

--- src/test/handshake_tests.cpp
+++ src/test/handshake_tests.cpp
@@ -7,10 +7,25 @@ BOOST_FIXTURE_TEST_SUITE(handshake_tests, BasicTestingSetup)
 
 BOOST_AUTO_TEST_CASE(bip324_handshake)
 {
-    p2p::Handshake hs;
-    std::array<unsigned char,32> peer{};
-    auto res = hs.Initiate(peer);
-    BOOST_CHECK(res.session_key.size() == 32);
+    p2p::Handshake initiator;
+    p2p::Handshake responder;
+    auto i_pub = initiator.GetPublicKey();
+    auto r_pub = responder.GetPublicKey();
+
+    auto i_res = initiator.Initiate(r_pub);
+    auto r_res = responder.Initiate(i_pub);
+
+    BOOST_CHECK(i_res.encryption_enabled);
+    BOOST_CHECK(r_res.encryption_enabled);
+    BOOST_CHECK_EQUAL_COLLECTIONS(i_res.send_key.begin(), i_res.send_key.end(), r_res.recv_key.begin(), r_res.recv_key.end());
+    BOOST_CHECK_EQUAL_COLLECTIONS(i_res.recv_key.begin(), i_res.recv_key.end(), r_res.send_key.begin(), r_res.send_key.end());
+
+    std::array<unsigned char,12> iv;
+    std::array<unsigned char,16> tag;
+    std::vector<unsigned char> msg = {0,1,2,3,4,5};
+    auto enc = p2p::Handshake::Encrypt(msg, i_res.send_key, iv, tag);
+    auto dec = p2p::Handshake::Decrypt(enc, r_res.recv_key, iv, tag);
+    BOOST_CHECK_EQUAL_COLLECTIONS(msg.begin(), msg.end(), dec.begin(), dec.end());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
