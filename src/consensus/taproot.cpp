3cb941d9e6ce3727dabfc17f073ee80ef83d246b
Mr-Miner <127097345+MrMiner-org@users.noreply.github.com>
2025-07-03 11:45:47 +0200

    Add BIP340 Schnorr and Taproot support

--- src/consensus/taproot.cpp
+++ src/consensus/taproot.cpp
@@ -1,13 +1,28 @@
 #include "taproot.h"
 #include <crypto/sha256.h>
 #include <hash.h>
+#include <schnorr.h>
+#include "script/script.h"
 
 namespace Consensus {
 bool VerifyTaprootSpend(const CTransaction& tx)
 {
-    // Placeholder Taproot verification
-    uint256 hash = Hash(tx.vin.begin(), tx.vin.end());
-    (void)hash; // suppress unused
-    return true;
+    if (tx.vin.empty() || tx.vout.empty()) return false;
+
+    const CTxIn& in = tx.vin[0];
+    if (in.scriptSig.size() != 64) return false;
+    std::array<unsigned char,64> sig;
+    std::copy(in.scriptSig.begin(), in.scriptSig.end(), sig.begin());
+
+    const CScript& spk = tx.vout[0].scriptPubKey;
+    if (spk.size() != 34 || spk[0] != OP_1) return false;
+    unsigned char pk[32];
+    memcpy(pk, &spk[1], 32);
+
+    uint256 h = tx.GetHash();
+    return crypto::SchnorrVerify(
+        std::span<const unsigned char,32>(h.begin(), 32),
+        std::span<const unsigned char,32>(pk, 32),
+        std::span<const unsigned char,64>(sig.data(), 64));
 }
 }
--- src/test/schnorr_tests.cpp
+++ src/test/schnorr_tests.cpp
@@ -7,9 +7,12 @@ BOOST_FIXTURE_TEST_SUITE(schnorr_tests, BasicTestingSetup)
 BOOST_AUTO_TEST_CASE(sign_verify)
 {
     unsigned char msg[32]{};
-    unsigned char key[32]{};
-    auto sig = crypto::SchnorrSign(std::span<const unsigned char,32>(msg,32), std::span<const unsigned char,32>(key,32));
-    bool ok = crypto::SchnorrVerify(std::span<const unsigned char,32>(msg,32), std::span<const unsigned char,32>(key,32), sig);
+    CKey key;
+    key.MakeNewKey(true);
+    auto sig = crypto::SchnorrSign(std::span<const unsigned char,32>(msg,32),
+                                   std::span<const unsigned char,32>(key.begin(),32));
+    bool ok = crypto::SchnorrVerify(std::span<const unsigned char,32>(msg,32),
+                                   std::span<const unsigned char,32>(key.GetPubKey().begin()+1,32), sig);
     BOOST_CHECK(ok);
 }
 
