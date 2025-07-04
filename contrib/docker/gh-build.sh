ab021bf998e12a8bbadee6bf6350baf86904d79a
Mr-Miner <127097345+MrMiner-org@users.noreply.github.com>
2025-07-04 20:15:09 +0200

    Add Docker build script

---
+++ contrib/docker/gh-build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+IMAGE_NAME=${IMAGE_NAME:-theminerzcoin}
+TAG=${GIT_CURRENT_BRANCH:-latest}
+
+# Build Docker image
+
+docker build -t "${IMAGE_NAME}:${TAG}" -f docker/Dockerfile .
+
+# Push Docker image
+
+docker push "${IMAGE_NAME}:${TAG}"
+
