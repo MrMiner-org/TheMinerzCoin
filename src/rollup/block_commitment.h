befb4935ddc6320bd7214d784c5790e1030a8c2f
Mr-Miner <127097345+MrMiner-org@users.noreply.github.com>
2025-07-04 16:52:58 +0200

    Add zk rollup scaffolding

---
+++ src/rollup/block_commitment.h
@@ -0,0 +1,11 @@
+#ifndef THEMZ_ROLLUP_BLOCK_COMMITMENT_H
+#define THEMZ_ROLLUP_BLOCK_COMMITMENT_H
+
+class BlockCommitment {
+public:
+    // Placeholder for block commitment data and operations
+    BlockCommitment() = default;
+    void record();
+};
+
+#endif // THEMZ_ROLLUP_BLOCK_COMMITMENT_H
